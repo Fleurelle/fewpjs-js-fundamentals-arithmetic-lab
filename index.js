1ea916c89be4905a71ecec00e10a5b1b362ccbf7
Rose <r.d.jeanlouis01@gmail.com>
2021-04-08 23:31:58 -0400

    commit tree 8c4020d9dbbf8ddfe794e44f1e01ef88e987a228

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// an equation whose multiplied results will equal 62
+const num1 = 7.8747;
+const num2 = 7.8747;
+const multiply = parseInt(num1*num2);
 
-// Write your code here
+//Generating a number greater than 0
+let random = Math.round(Math.random());
+
+//an equation that calculates a remainder that is equal to 4
+const num3 = 19;
+const num4 = 5;
+const mod = parseInt(num3 % num4);
+
+//will return 20 as the highest number in the set
+let max = Math.max(1,2,3,4,5,10,15,20);
